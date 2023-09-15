b913a04cb54117963006b855972ae1bcfd760fab
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-09-15 16:26:41 +0800

    15/9. creates layout

--- app/src/main/java/com/example/sqlite_demo/MainActivity.java
+++ app/src/main/java/com/example/sqlite_demo/MainActivity.java
@@ -3,14 +3,76 @@ package com.example.sqlite_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTxtName, editTxtAge;
+    private Switch switch_activeCustomer;
+    private Button btnViewAll, btnAdd;
+    private ListView customer_listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //init views
+        initViews();
+
+        //on click add
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                try {
+
+                //get from edit text
+                int id= -1;
+                String name= editTxtName.getText().toString();
+                int age= Integer.parseInt( editTxtAge.getText().toString() );
+                boolean isChecked= switch_activeCustomer.isChecked();
+
+
+                    CustomerModel customerModel= new CustomerModel(id, name, age, isChecked );
+
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "error creating customer", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        //onclick view all
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "View all", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    //init
+    private void initViews(){
+        editTxtName= findViewById(R.id.editTxtName);
+        editTxtAge= findViewById(R.id.editTxtAge);
+
+        switch_activeCustomer= findViewById(R.id.switch_activeCustomer);
+
+        btnViewAll= findViewById(R.id.btnViewAll);
+        btnAdd= findViewById(R.id.btnAdd);
+
+        customer_listView= findViewById(R.id.customer_listView);
 
     }
 }
